e89434b18b7e5e258f355cb5d00084f752855555
Sepopur0 <donghuongmen@gmail.com>
2023-12-13 15:13:25 +0700

    nhuong Tu

--- src/apis/login/login.js
+++ src/apis/login/login.js
@@ -4,5 +4,16 @@ export const LoginAPI = {
     post: (value) => {
         const url = `/login`;
         return axiosClient.post(value);
+    },
+    get: (id) => {
+        const url = `/employee/login/${id}`;
+        return axiosClient.get(id);
+    }
+}
+
+export const GetInfoAPI = {
+    get: (id) => {
+        const url = `/employee/${id}`;
+        return axiosClient.get(id);
     }
 }
